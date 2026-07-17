42d2f7e34d95466a3e979eb0b26f256022d2baba
orewapantherdprabhu <dhanushkorubelli@gmail.com>
2026-07-17 15:10:37 +0000

    Add fade-up animation for collection section and enhance styling

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ window.addEventListener("scroll", () => {
     }
 
 });
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".fade-up").forEach((element)=>{
+    observer.observe(element);
+});
