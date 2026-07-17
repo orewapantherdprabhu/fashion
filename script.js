431756f7403acd4cf68c2e62c5ae4178102f8314
orewapantherdprabhu <dhanushkorubelli@gmail.com>
2026-07-17 15:39:39 +0000

    Refactor IntersectionObserver logic for improved readability

--- script.js
+++ script.js
@@ -16,13 +16,25 @@ window.addEventListener("scroll", () => {
 
 });
 const observer = new IntersectionObserver((entries) => {
+
     entries.forEach(entry => {
+
         if(entry.isIntersecting){
+
             entry.target.classList.add("show");
+
+        }else{
+
+            entry.target.classList.remove("show");
+
         }
+
     });
+
 });
 
 document.querySelectorAll(".fade-up").forEach((element)=>{
+
     observer.observe(element);
+
 });
